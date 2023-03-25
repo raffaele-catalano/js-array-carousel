249c42f73fd5ad9a5b887c8a7744906a7728b685
raffaele-catalano <raf.catalano@gmail.com>
2023-03-25 18:51:13 +0100

    added circles & circles behaviour

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ const pictures = [
 
 // elements
 const picsWrapper = document.querySelector('.pics-wrapper');
+const circlesWrapper = document.querySelector('.circles');
 const arrowNext = document.querySelector('.next');
 const arrowPrev = document.querySelector('.prev');
 
@@ -22,21 +23,30 @@ for (let i = 0; i < pictures.length; i++) {
     const pic = pictures[i];
 
     picsWrapper.innerHTML += `<img class="pic hide" src="${pic}">`
+
+    circlesWrapper.innerHTML += `<div class="circle"></div>`
 }
 
 const items = document.getElementsByClassName('pic');
+    items[counterPic].classList.remove('hide');
     // console.log('raccolta delle immagini per classe', items);.
 
+const circlesGroup = document.getElementsByClassName('circle');
+    circlesGroup[counterPic].classList.add('active');
 
 arrowPrev.classList.add('hide');
 
 
 arrowNext.addEventListener('click', function() {
 
+    circlesGroup[counterPic].classList.remove('active');
+
     items[counterPic].classList.add('hide');
 
     counterPic++;
     items[counterPic].classList.remove('hide');
+
+    circlesGroup[counterPic].classList.add('active');
 
     if (counterPic == pictures.length - 1) {
         arrowNext.classList.add('hide');
@@ -47,12 +57,15 @@ arrowNext.addEventListener('click', function() {
 })
 
 arrowPrev.addEventListener('click', function() {
+
+    circlesGroup[counterPic].classList.remove('active');
     
     items[counterPic].classList.add('hide');
 
     counterPic--;
     items[counterPic].classList.remove('hide');
 
+    circlesGroup[counterPic].classList.add('active');
 
     if (counterPic === 0) {
         arrowPrev.classList.add('hide');
