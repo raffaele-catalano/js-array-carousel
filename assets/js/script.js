36aefcc4bb246e1afa4d62e01a2911ba10507c3c
raffaele-catalano <raf.catalano@gmail.com>
2023-03-25 18:04:13 +0100

    added click event to arrows

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,16 @@ const pictures = [
     'assets/img/05.webp',
 ]
 
-    console.log('array picture ->',pictures);
+    // console.log('array picture ->',pictures);
 
-
-
-// 1
+// elements
 const picsWrapper = document.querySelector('.pics-wrapper');
+const arrowNext = document.querySelector('.next');
+const arrowPrev = document.querySelector('.prev');
+
+    // contatore
+let counterPic = 0;
+
 
 for (let i = 0; i < pictures.length; i++) {
 
@@ -22,3 +26,24 @@ for (let i = 0; i < pictures.length; i++) {
     <img class="pic hide" src="${pic}">
     `
 }
+
+const items = document.getElementsByClassName('pic');
+    // console.log('raccolta delle immagini per classe', items);.
+
+arrowNext.addEventListener('click', function() {
+
+    items[counterPic].classList.add('hide');
+
+    counterPic++;
+    items[counterPic].classList.remove('hide');
+
+})
+
+arrowPrev.addEventListener('click', function() {
+    
+    items[counterPic].classList.add('hide');
+
+    counterPic--;
+    items[counterPic].classList.remove('hide');
+
+})
